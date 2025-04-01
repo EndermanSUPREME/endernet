3da60dbb84f0b75821b92bc6e76c941dc1ae2a07
Ender <67215373+EndermanSUPREME@users.noreply.github.com>
2025-04-01 19:52:02 -0400

    readded script.js in attempt to resolve navbar bug

---
+++ js/script.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const projectListsBlock = `
+        <li id="dropdownTitle">General</li>
+        <li><a class="dropdown-item" href="smiirlcounter.html">Hardware Hack</a></li>
+        <li><a class="dropdown-item" href="untitled_wedge.html">Untitled Wedge</a></li>
+        <li>
+            <hr class="dropdown-divider">
+        </li>
+        <li id="dropdownTitle">Research</li>
+        <li><a class="dropdown-item" href="srcSlice.html">srcSlice</a></li>
+        <li>
+            <hr class="dropdown-divider">
+        </li>
+        <li id="dropdownTitle">Cyber Security</li>
+        <li><a class="dropdown-item" href="windowsKeyLogger.html">Windows Keylogger</a></li>
+        <li><a class="dropdown-item" href="c2_server.html">C2 Server</a></li>
+    `;
+
+    // Look for the element with id="InterestingProjects" and insert the html block above
+    // into the content of the tag found, the snippet above is used in multiple pages
+    document.getElementById("InterestingProjects").innerHTML = projectListsBlock;
+});
