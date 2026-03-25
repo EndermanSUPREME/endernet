80bebe204a708a87beff4aae950ede90a36fda04
Ender <nallahabi2003@gmail.com>
2026-03-25 11:47:36 -0400

    Added link to git-org

--- js/script.js
+++ js/script.js
@@ -1,15 +1,35 @@
 function generateLinks() {
-    const A = [94,0,18,6,29,89,77,70,7,25,5,23,83,25,25,95,0,5,30,64,10,13,70];
-    let b = "76tfvncbifwwr7t";
-    let r = ""; let j = 0;
-    for (let i = 0; i < A.length; ++i) {
-        j = ++j % b.length;
-        r += String.fromCharCode(A[i] ^ b.charCodeAt(j));
+    {
+        const A = [94,0,18,6,29,89,77,70,7,25,5,23,83,25,25,95,0,5,30,64,10,13,70];
+        let b = "76tfvncbifwwr7t";
+        let r = ""; let j = 0;
+        for (let i = 0; i < A.length; ++i) {
+            j = ++j % b.length;
+            r += String.fromCharCode(A[i] ^ b.charCodeAt(j));
+        }
+        const a1 = document.getElementById("itch-io"); const a2 = document.getElementById("itch-io-chat");
+        const a3 = document.getElementById("itch-io-chat2"); const a4 = document.getElementById("itch-io-adenia");
+        if (a1) a1.href = r; if (a2) a2.href = r + "chat";
+        if (a3) a3.href = r + "chat-2"; if (a4) a4.href = r + "adenia";
     }
-    const a1 = document.getElementById("itch-io"); const a2 = document.getElementById("itch-io-chat");
-    const a3 = document.getElementById("itch-io-chat2"); const a4 = document.getElementById("itch-io-adenia");
-    if (a1) a1.href = r; if (a2) a2.href = r + "chat";
-    if (a3) a3.href = r + "chat-2"; if (a4) a4.href = r + "adenia";
+
+    {
+        const C_e = [0, 22,  2,  2, 70, 12, 86, 71, 13,
+                    81, 67, 94, 66, 10, 76, 21, 29, 88,
+                    25, 44,  6, 46, 93, 81, 95, 89, 13,
+                    6, 53, 33];
+        const b2 = "7hbvr56yhj876";
+        let r2 = ""; let j2 = 0;
+        for (const e of C_e) {
+            j2  = ++j2 % b2.length;
+            r2 += String.fromCharCode(e ^ b2.charCodeAt(j2));
+        }
+        const p1 = document.getElementById("cyber-sec-summ");
+        if (p1) {
+            const bE = "SSBkbyB2YXJpb3VzIHByb2plY3RzIGluIG15IDxhIGhyZWY9IiR7cjJ9Ij5wZXJzb25hbCBvcmdhbml6YXRpb248L2E+LiBQcm9qZWN0cyBpbmNsdWRlIHNtYWxsIHRvb2xzLCBiYXNpYyBtYWx3YXJlIHByb2plY3RzIGZvciB0ZWFjaGluZywgYW5kIGV4cGxvaXRhdGlvbi4=";
+            p1.innerHTML += ` ${atob(bE)}`;
+        }
+    }
 }
 
 document.addEventListener("DOMContentLoaded", function () {
